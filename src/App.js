92dccc4de8c57e925128341157b9d979fdb8cd1d
shubhanshurav <shubhanshurao7037@gmail.com>
2023-09-29 22:28:45 +0530

    Add Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./style/index.css";
+import HomePage from "./components/HomePage";
+import Shop from "./components/Shop";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 const App = () => {
-  return <div>App</div>;
+  return(
+    <div>
+       <HomePage />
+       <Shop />
+       <About />
+       <Contact />
+    </div>
+  );
 };
 
 export default App;
