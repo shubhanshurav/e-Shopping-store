7b9dfaa125f346501cbe983b45be11271b54aef4
SAKETSINGH120 <saketsingh111222@gmail.com>
2023-10-08 11:25:58 +0530

    login Modification

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,8 +8,8 @@ const Login = () => {
   <form action="" className='flex  flex-col  bg-slate-50 m-auto  place-content-center rounded-xl  items-center w-[350px]  border-slate-200
   border-2  '>
 
-<div className='m-10'><img  src='./assets/Login img.jpg' className='text-[43px]  text-black font-semibold w-[100px] ' alt="logo"/>
-{/* <p className='text-[15px] text-slate-500 font-thin' >Welcome back....</p> */}
+<div className='m-5'><img  src='./assets/Login img.jpg' className='text-[43px]  text-black font-semibold w-[100px] ' alt="logo"/>
+<p className='text-[15px] text-slate-500 font-thin' >Welcome back....</p>
 </div >
 
 
@@ -22,9 +22,25 @@ name='email'
 placeholder='Enter Your Password' 
 name='Password'
  />
-<Link to="/ForgetPassword" className='text-[13px] flex  gap-[95px] p-2 text-red-500 font-semibold '>Forget Password <Link to="/Signup"><span className='text-[15px] font-semibold text-green-500'>Sign-up</span></Link></Link>
 
-<button className='w-[100px] py-1 border  mb-12 mt-8 bg-slate-400 text-green-200 font-semibold rounded-md hover:bg-slate-200 hover:text-black'>Login</button>
+<div className='flex flex-col m-auto'>
+        <Link
+          to="/ForgetPassword"
+          className="text-[13px] flex mr-1 p-2 text-red-500 font-semibold"
+        >
+          Forget Password {""}
+        </Link>
+          <div className='flex flex-row mr-5 justify-between gap-7'>
+            <p>Dont have an account?</p>
+          <Link to="/Signup">
+            <p className="text-[15px] font-semibold text-green-500">
+              Sign-up
+            </p>
+          </Link>
+          </div>
+        </div>
+
+<button className='w-[100px] py-1 border  mb-12 mt-8 bg-blue-500 text-black font-semibold rounded-md hover:bg-slate-200 hover:text-black'>Login</button>
   </form>
   </div>
   )
